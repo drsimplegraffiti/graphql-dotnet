2e64cbd87ec72d3950d95811e6af3eef54c9cd22
Ogunnusi Abayomi Joseph <70065792+drsimplegraffiti@users.noreply.github.com>
2023-11-20 08:31:16 +0100

    added hang fire

--- GraphQlApi/Program.cs
+++ GraphQlApi/Program.cs
@@ -2,15 +2,25 @@ using GraphQlApi.Data;
 using GraphQlApi.GraphQL.Mutations;
 using GraphQlApi.GraphQL.Types;
 using GraphQlApi.Repositories;
+using Hangfire;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//Register Service
+
+
+
+//Register Servic
 builder.Services.AddScoped<IProductService, ProductService>();
 //InMemory Database
-builder.Services.AddDbContext<DbContextClass>(o => o.UseInMemoryDatabase("GraphQLDemo"));
+// builder.Services.AddDbContext<DbContextClass>(o => o.UseInMemoryDatabase("GraphQLDemo"));
+//SQL Server Database
+builder.Services.AddDbContext<DbContextClass>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Hangfire
+builder.Services.AddHangfire(x => x.UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddHangfireServer();
 
 //GraphQL Config
 builder.Services.AddGraphQLServer()
@@ -44,6 +54,9 @@ app.MapGraphQL();
 //REST API
 
 app.UseHttpsRedirection();
+
+//Hangfire
+app.UseHangfireDashboard();
 
 app.UseAuthorization();
 
